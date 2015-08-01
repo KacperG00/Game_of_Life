3b304d4af25377b97830d19ccbd563c59a933900
KacperG00 <kacper-gruszka60@wp.pl>
2015-08-01 20:36:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+int nRandom;
+
+void drukujTablice(bool tablica[][10])
+{
+    for(int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            nRandom=rand() %100 +1;
+            if(nRandom<26)  tablica[i][j]=1;
+            else            tablica[i][j]=0;
+
+            if(tablica[i][j]==0)   cout<<' ';
+            else                   cout<<'*';
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello wdsafsorld2!" << endl;
+    srand(time(NULL));
+    bool tablica[10][10]={};
+    drukujTablice(tablica);
     return 0;
 }
