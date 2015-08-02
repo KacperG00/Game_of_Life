8bed4ef44f76c0569995dab6047ed5c9b016bb37
KacperG00 <kacper-gruszka60@wp.pl>
2015-08-02 04:02:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,38 @@ using namespace std;
 
 int nRandom;
 
-void drukujTablice(bool tablica[][10])
+void los(bool tablica[][10])
 {
     for(int i=0; i<10; i++)
     {
         for(int j=0; j<10; j++)
         {
             nRandom=rand() %100 +1;
-            if(nRandom<26)  tablica[i][j]=1;
-            else            tablica[i][j]=0;
-
-            if(tablica[i][j]==0)   cout<<' ';
-            else                   cout<<'*';
+            if(nRandom<26)  {tablica[i][j]=1;}//miały być klamry
+            else            {tablica[i][j]=0;}// :D
         }
-        cout<<endl;
     }
+}
+
+void drukujTablice(bool tablica[][10])
+{
+    system("cls");
+    for(int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            if(tablica[i][j]==0)   cout<<' ';  //Jak dla mnie to
+            else                   cout<<'*';  //nie wygląda
+        }                                      //źle...
+        cout<<endl;
+    }//... a szkoda linijek na klamerki :P
 }
 
 int main()
 {
     srand(time(NULL));
     bool tablica[10][10]={};
+    los(tablica);
     drukujTablice(tablica);
     return 0;
 }
