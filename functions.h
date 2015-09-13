656e07e5620af7435c5e2026b1b9ce4b1ffded28
Kacper Gruszka <kacper@kg.pc>
2015-09-13 19:24:49 +0200

    Nagłówki

---
+++ functions.h
@@ -0,0 +1,9 @@
+bool ** utworzTablice(int nY, int nX);
+void usunTablice(bool ** tablica, int nY);
+void glider(bool **tablica, int nY, int nX);
+void los(bool **tablica, int nY, int nX);
+int mod (int a, int b);
+void przeliczGeneracjeNaKomorce(bool **tablica, bool **bTymczasowa, int nY, int nX);
+bool ** kolejnaGeneracja(bool **tablica, bool **bTymczasowa, int nY, int nX);
+void drukujTablice(bool **tablica, int nY, int nX);
+bool czyKonczyc(int i);
