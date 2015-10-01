95fe3df2adc6592554207c0b968b800b3c100222
Kacper Gruszka <kacper@kg.pc>
2015-10-01 22:40:52 +0200

    Zwalnianie, przyśpieszanie, zatrzymywanie

--- main.cpp
+++ main.cpp
@@ -1,29 +1,57 @@
-#include "functions.cpp"
+#include "functions.h"
+#include "non-blocking.cpp"
 
 using namespace std;
 
 int main()
 {
-    unsigned short int nY=20, nX=70;
+    unsigned short int nY=20, nX=70, nrGeneracji=0;
+    short int V=1;
     srand(time(NULL));
 
     bool ** tablica = utworzTablice(nY, nX);
 
     los(tablica, nY, nX);
     //glider(tablica, nY, nX);
-    for(int i=1; ; i++)
+    
+    char c;
+    int i=0;
+    
+    nonblock(NB_ENABLE);
+    while(!i)
     {
-        drukujTablice(tablica, nY, nX);
+		nrGeneracji++;
+		usleep(100000*V);
+		i=kbhit();
+		if(i!=0)
+		{
+			c=fgetc(stdin);
+			if(c=='q')
+			{
+				usunTablice(tablica, nY);
+				system("^C");
+			}
+			else if(c=='s')
+			{
+				getchar();
+				i=0;
+			}
+			else if(c=='+')
+			{
+				V++;
+				i=0;
+			}
+			else if(c=='-')
+			{
+				V-=1;
+				i=0;
+			}
+		}
+		drukujTablice(tablica, nY, nX, nrGeneracji);
         bool ** bTymczasowa = utworzTablice(nY, nX);
         bTymczasowa = kolejnaGeneracja(tablica, bTymczasowa, nY, nX);
         usunTablice(tablica, nY);
         tablica = bTymczasowa;
-        usleep(100000);
-        if(czyKonczyc(i)==1)
-        {
-            usunTablice(tablica, nY);
-            return 0;
-        }
-    }
+	}
+    nonblock(NB_DISABLE);
 }
-
