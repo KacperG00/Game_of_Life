b9fd655927c117e6778dc6391a8a67d6b79ee50b
Kacper Gruszka <kacper@kg.pc>
2015-11-06 19:34:07 +0100

    Przyspieszanie, zwalnianie, zatrzymywanie częstości drukowania

--- main.cpp
+++ main.cpp
@@ -5,53 +5,61 @@ using namespace std;
 
 int main()
 {
-    unsigned short int nY=20, nX=70, nrGeneracji=0;
-    short int V=1;
+    unsigned short int nY=20, nX=70;
+    float V=1;
     srand(time(NULL));
 
     bool ** tablica = utworzTablice(nY, nX);
 
-    los(tablica, nY, nX);
-    //glider(tablica, nY, nX);
-    
+    //los(tablica, nY, nX);
+    glider(tablica, nY, nX);
+
     char c;
     int i=0;
+    unsigned long int iterator=0;
     
     nonblock(NB_ENABLE);
     while(!i)
     {
-		nrGeneracji++;
-		usleep(100000*V);
+		iterator++;
+
 		i=kbhit();
 		if(i!=0)
 		{
 			c=fgetc(stdin);
-			if(c=='q')
+			switch(c)
 			{
-				usunTablice(tablica, nY);
-				system("^C");
+				case (char)27: usunTablice(tablica, nY); return 0;	//ESC
+				case (char)32: getchar(); break;	//Spacebar
+				case '+': V+=1; break;
+				case '-': V-=1; if(V==0) {getchar(); V+=1;} break;
+				case '*': V=V*2; break;
+				case '/': V=V/2; break;
+				case '1': V=1; break;
+				case '2': V=5; break;
+				case '3': V=10; break;
+				case '4': V=100; break;
 			}
-			else if(c=='s')
-			{
-				getchar();
-				i=0;
-			}
-			else if(c=='+')
-			{
-				V++;
-				i=0;
-			}
-			else if(c=='-')
-			{
-				V-=1;
-				i=0;
-			}
+			
+			i=0;
 		}
-		drukujTablice(tablica, nY, nX, nrGeneracji);
-        bool ** bTymczasowa = utworzTablice(nY, nX);
-        bTymczasowa = kolejnaGeneracja(tablica, bTymczasowa, nY, nX);
-        usunTablice(tablica, nY);
-        tablica = bTymczasowa;
+		if(iterator%5==0)
+		{
+			if(V<100)
+			{
+				drukujTablice(tablica, nY, nX, iterator);
+			}
+			else if(iterator%25==0)
+			{
+				drukujTablice(tablica, nY, nX, iterator);
+			}
+			bool ** bTymczasowa = utworzTablice(nY, nX);
+			bTymczasowa = kolejnaGeneracja(tablica, bTymczasowa, nY, nX);
+			usunTablice(tablica, nY);
+			tablica = bTymczasowa;
+			
+			usleep(1000000/V);
+		}
 	}
     nonblock(NB_DISABLE);
 }
