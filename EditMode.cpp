d46ffd3b72fef9cc10147e3e0bd41af8fbe6b6ce
Kacper Gruszka <kacper@kg.pc>
2015-11-15 12:11:33 +0100

    Tryb edycji

---
+++ EditMode.cpp
@@ -0,0 +1,53 @@
+#include "functions.h"
+#include "non-blocking.cpp"
+
+using namespace std;
+
+int edytuj(bool **tablica, int nY, int nX, unsigned long int nrGeneracji)
+{
+	int Y=0, X=0;
+	
+	unsigned int iterator=0;
+	
+	char c;
+    int i=0;
+    nonblock(NB_ENABLE);
+	while(!i)
+    {
+		if(iterator%2==0)
+		{
+			if(iterator%10<5)
+			{
+				drukujTabliceEdycja(tablica, nY, nX, nrGeneracji, Y, X);
+			}
+			else if(iterator%10>=5)
+			{
+				drukujTablice(tablica, nY, nX, nrGeneracji);
+			}
+		}
+		
+		i=kbhit();
+		if(i!=0)
+		{
+			c=fgetc(stdin);
+			switch(c)
+			{
+				case 'w': if(Y>0)Y=Y-1; break;
+				case 's': if(Y<nY-1)Y=Y+1; break;
+				case 'a': if(X>0)X=X-1; break;
+				case 'd': if(X<nX-1)X=X+1; break;
+				case 'z': if(tablica[Y][X]==1) tablica[Y][X]=0;
+							else if(tablica[Y][X]==0) tablica[Y][X]=1;
+					break;
+				case (char)32: return 0; break;
+				case (char)27: usunTablice(tablica, nY); return 0; break;
+			}
+			
+			i=0;
+		}
+		usleep(100000);
+		iterator++;
+	}
+	nonblock(NB_DISABLE);
+	return 0;
+}
