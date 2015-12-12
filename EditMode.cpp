565806dc079f588dc9d292956cccfb1980402d86
KacperG00 <kacper-gruszka60@wp.pl>
2015-12-12 22:56:44 +0100

    Update EditMode.cpp

--- EditMode.cpp
+++ EditMode.cpp
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 #include <cstdlib>
 #include <stdio.h>
 #include <unistd.h>
 #include <ctime>
-=======
 #include <iostream>
->>>>>>> f84cb32387f86a6b3b4c4e46274b6d7ccbb19c72
 #include "EditMode.h"
 #include "functions.h"
 #include "non-blocking.h"
@@ -25,12 +22,10 @@ int edytuj(bool **tablica, int nY, int nX, unsigned long int nrGeneracji)
     {
 		if(iterator%2==0)
 		{
-<<<<<<< HEAD
 			if((iterator%10<5)||(iterator%10>=5))
 			{
 				drukujTabliceEdycja(tablica, nY, nX, nrGeneracji, Y, X);
 			}
-=======
 			if(iterator%10<5)
 			{
 				drukujTabliceEdycja(tablica, nY, nX, nrGeneracji, Y, X);
@@ -39,7 +34,6 @@ int edytuj(bool **tablica, int nY, int nX, unsigned long int nrGeneracji)
 			{
 				drukujTablice(tablica, nY, nX, nrGeneracji);
 			}
->>>>>>> f84cb32387f86a6b3b4c4e46274b6d7ccbb19c72
 		}
 		
 		i=kbhit();
@@ -53,17 +47,14 @@ int edytuj(bool **tablica, int nY, int nX, unsigned long int nrGeneracji)
 				case 'a': if(X>0)X=X-1; break;
 				case 'd': if(X<nX-1)X=X+1; break;
 				case 'z': if(tablica[Y][X]==1) tablica[Y][X]=0;
-<<<<<<< HEAD
 							else if(tablica[Y][X]==0) tablica[Y][X]=1; break;
 				case 'l': los(tablica, nY, nX); break;
 				case (char)32: return 0; break;
 				case (char)27: return 1; break;
-=======
 							else if(tablica[Y][X]==0) tablica[Y][X]=1;
 					break;
 				case (char)32: return 0; break;
 				case (char)27: usunTablice(tablica, nY); return 0; break;
->>>>>>> f84cb32387f86a6b3b4c4e46274b6d7ccbb19c72
 			}
 			
 			i=0;
